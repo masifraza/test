2f8ff81f1c964662a15fabe7c84e9c8196f87484
unknown <Administrator@DESKTOP-H37KJUM.DHA.COM.PK>
2023-12-29 10:54:39 +0500

    original file kot.php

---
+++ kot.php
@@ -0,0 +1,20 @@
+<?php
+$arr = array('one','two','three','four','five');
+$html = "<div>";
+foreach($arr as $value){
+    $html .= $value.'<br />';
+}
+$html .= "</div>"; 
+
+?>
+
+<script type="text/javascript">     
+    function PrintDiv() {    
+		alert("printer...");
+       var divToPrint = document.getElementById('divToPrint');
+       var popupWin = window.open('', '_blank', 'width=300,height=300');
+       popupWin.document.open();
+       popupWin.document.write('<html><body onload="window.print()">' + divToPrint.innerHTML + '</html>');
+        popupWin.document.close();
+            }
+ </script>
